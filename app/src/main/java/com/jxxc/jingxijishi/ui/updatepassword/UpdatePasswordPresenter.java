fb0f58ba7f6230fd83dd61f34638286e2349087d
lienyin <lienyin@zhizukj.com>
2019-12-29 15:56:00 +0800

    添加

--- app/src/main/java/com/jxxc/jingxijishi/ui/updatepassword/UpdatePasswordPresenter.java
+++ app/src/main/java/com/jxxc/jingxijishi/ui/updatepassword/UpdatePasswordPresenter.java
@@ -2,6 +2,7 @@ package com.jxxc.jingxijishi.ui.updatepassword;
 
 import android.content.Context;
 
+import com.jxxc.jingxijishi.http.EventCenter;
 import com.jxxc.jingxijishi.mvp.BasePresenterImpl;
 
 /**
@@ -10,5 +11,9 @@ import com.jxxc.jingxijishi.mvp.BasePresenterImpl;
  */
 
 public class UpdatePasswordPresenter extends BasePresenterImpl<UpdatePasswordContract.View> implements UpdatePasswordContract.Presenter{
-    
+
+    @Override
+    protected void onEventComing(EventCenter center) {
+        
+    }
 }
