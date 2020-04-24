d84a5d467e7826d309412d6708e047b05ef8e84f
lienyin <lienyin@zhizukj.com>
2020-04-24 18:05:26 +0800

    添加

--- app/src/main/java/com/jxxc/jingxijishi/Api.java
+++ app/src/main/java/com/jxxc/jingxijishi/Api.java
@@ -3,10 +3,7 @@ package com.jxxc.jingxijishi;
 import com.jxxc.jingxijishi.utils.SPUtils;
 
 public class Api {
-//    public static final String BASEURL = "https://repair-api-sit.zhizukj.com/zhizu/repair";//测试环境
-//    public static final String BASEURL = "https://zhizuxia.zhizukj.com/ydb/app/";//生产
-   public static final String BASEURL = "https://jx.bashideban.com/technician/";//
-   //public static final String BASEURL = "http://192.168.0.59:8100/";//于立华
+   public static final String BASEURL = "https://jx.incofuture.com/technician/";
 
     //1-客户登录
     public static final String LOGIN = BASEURL + "system/technician/login";
